300649180eb45788a5cc0e6c3d0bd4846c398ea8
Eric Ponce <ponceeric@gmail.com>
2012-03-06 00:29:40 +0100

    Categories styles updated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.where('category_id <> 3 AND category_id <> 4').order('id DESC')
     @categories = Category.order('name').all
-    @articles_sport = Article.where(:category_id => 3).order('id DESC')
+    @articles_sport = Article.where(:category_id => Configuration.value(:main_sports_category)).order('id DESC')
     @articles_opinion = Article.where(:category_id => 4).order('id DESC')
 
     respond_to do |format|
