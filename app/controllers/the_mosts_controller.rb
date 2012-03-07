17e8cd66cc53632d2363514c9eb5cce71ae17425
Eric Ponce <ponceeric@gmail.com>
2012-03-08 00:56:29 +0100

    The_mosts page fixed

--- app/controllers/the_mosts_controller.rb
+++ app/controllers/the_mosts_controller.rb
@@ -1,7 +1,7 @@
 class TheMostsController < ApplicationController
   def index
-    @articles_viwed = Article.first
-    @articles_commented = Article.damedato
+    @articles_commented = Article.get_comments(:comments_count)
+    @articles_viewed = Article.get_comments
     
     respond_to do |format|
       format.html # index.html.erb
