126461e8e8eb44cc3b96688cfe5d3dab81abd313
Eric Ponce <ponceeric@gmail.com>
2012-03-16 10:43:30 +0100

    Updated test

--- test/integration/articles_integration_test.rb
+++ test/integration/articles_integration_test.rb
@@ -7,6 +7,25 @@ describe "Articles integration" do
   end
   
   describe "Home" do
+    
+    describe "general" do
+      it "should load the principal page" do
+        visit '/'
+        assert page.has_content?('News').must_be_same_as true
+      end
+      
+      it "should load the admin page" do
+        skip
+        visit '/admin'
+        fill_in('Email', :with => 'admin@example.com')
+        fill_in('Password', :with => 'password')
+        click_button('Login')
+        
+        assert current_path.must_be_same_as '/admin/article_comments'
+      end
+      
+    end
+    
     describe "Published" do
       it "shouldn't show the article if not published" do
         visit '/'
@@ -14,7 +33,8 @@ describe "Articles integration" do
       end
 
       it "should show the article if its published" do
-        @article = Article.create :title => 'My article test', :body => 'The body of the test article', :category => Category.first, :published_date => '2012-03-11', :published => true
+        @article = Article.create :title => 'My article test', :body => 'The body of the test article',
+         :category => Category.first, :published_date => '2012-03-11', :published => true
         visit '/'
         assert page.has_content?('My article test').must_be_same_as true
       end
