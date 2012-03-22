b96283ce53f77433fe09aa4dad87c7968ab9109b
Eric Ponce <ponceeric@gmail.com>
2012-03-23 00:43:17 +0100

    Changed default languaje to EN

--- config/application.rb
+++ config/application.rb
@@ -62,5 +62,19 @@ module Pdigital
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "yourdomain.dev",
+      :user_name            => "from@yourdomain.dev",
+      :password             => "Super-Secure-Password",
+      :authentication       => :plain,
+      :enable_starttls_auto => true
+    }
+
+    config.action_mailer.default_url_options = {
+      :host => "yourdomain.dev"
+    }
   end
 end
