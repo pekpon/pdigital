c07f14b4a0ceef2431dbc8216313cbeb21b79af7
Eric Ponce <ponceeric@gmail.com>
2012-04-16 18:37:15 +0200

    Some changes

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,7 +10,20 @@ class Article < ActiveRecord::Base
       self.slug.blank? or (new_record? and self.slug.blank?)
   end	
   
+  #paperclip image
 	has_attached_file :image, :styles => { :detail => "610x610>", :medium => "330x330>", :thumb => "100x100>" },
+      :url => "/system/images/:id/:style/:basename.:extension",          
+      :path => ":rails_root/public/system/images/:id/:style/:basename.:extension"
+  #paperclip image 2
+  has_attached_file :image_2, :styles => { :detail => "610x610>", :medium => "330x330>", :thumb => "100x100>" },
+      :url => "/system/images/:id/:style/:basename.:extension",          
+      :path => ":rails_root/public/system/images/:id/:style/:basename.:extension"
+  #paperclip image 3
+  has_attached_file :image_3, :styles => { :detail => "610x610>", :medium => "330x330>", :thumb => "100x100>" },
+      :url => "/system/images/:id/:style/:basename.:extension",          
+      :path => ":rails_root/public/system/images/:id/:style/:basename.:extension"
+  #paperclip image 4
+  has_attached_file :image_4, :styles => { :detail => "610x610>", :medium => "330x330>", :thumb => "100x100>" },
       :url => "/system/images/:id/:style/:basename.:extension",          
       :path => ":rails_root/public/system/images/:id/:style/:basename.:extension"
 	
