3186db7c144f10be98de5ed1b11f461cae5d7d9e
Eric Ponce <ponceeric@gmail.com>
2012-04-23 17:52:05 +0200

    Added RSS

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -89,4 +89,10 @@ class ArticlesController < ApplicationController
       format.js
     end
   end
+  
+  def rss
+    @articles = Article.find(:all, :order => "id DESC", :limit => 10)
+    render :layout => false
+    response.headers["Content-Type"] = "application/xml; charset=utf-8"
+  end
 end
