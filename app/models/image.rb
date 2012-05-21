3b76443338083a2e774231890440ee66ba3a8505
Eric Ponce <ponceeric@gmail.com>
2012-05-21 14:05:23 +0200

    Added facebook image to article

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,7 +4,7 @@ class Image < ActiveRecord::Base
   validates :image_foot, :length => { :maximum => 250 }
   
   #paperclip image
-	has_attached_file :image, :styles => { :detail => "610x610>", :medium => "330x330>", :thumb => "90x90#" },
+	has_attached_file :image, :styles => { :detail => "610x610>", :medium => "330x330>", :facebook => "200x200#", :thumb => "90x90#" },
       :url => "/system/images/:id/:style/:basename.:extension",          
       :path => ":rails_root/public/system/images/:id/:style/:basename.:extension"
 end
