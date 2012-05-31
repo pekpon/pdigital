06ce4bee39d3012a25deece45e78b451c6fe57bb
Eric Ponce <ponceeric@gmail.com>
2012-06-01 01:56:20 +0200

    Updated RealTime

--- app/helpers/real_time_helper.rb
+++ app/helpers/real_time_helper.rb
@@ -1,95 +1,107 @@
 module RealTimeHelper
   
-  def vote_multiple(votes,object)
-    count = 0
+  def objectOk(object)
     
-		votes.each do |vote|
-		  if vote.comment.nil?
-	    else
-        if vote.comment.commentable == object.comment.commentable
-		      count = count + 1
-		    end
-	    end
-	  end
-	  count
-  end
-  
-  def getPath(object)
-    if object.class.to_s == 'Article' or object.class.to_s == 'Debate' or object.class.to_s == 'Event'
-      type = object.class.to_s
-      obj = object
-    elsif object.class.to_s == 'Comment'
-      type = object.commentable.class.to_s
-      obj = object.commentable
-    else
-      type = object.comment.commentable.class.to_s
-      obj = object.comment.commentable
-    end
-
-    path = article_path(obj)  if type == 'Article'
-    path = debate_path(obj)  if type == 'Debate'
-    path = event_path(obj)  if type == 'Event'
+    result = false
     
-    return path
-  end
-  
-  def getText(object,var)
-    if object.class.to_s == 'Article' or object.class.to_s == 'Debate' or object.class.to_s == 'Event'
-      type = object.class.to_s
-      t = "new_"
-      obj = object
-    elsif object.class.to_s == 'Comment'
-      type = object.commentable.class.to_s
-      obj = object.commentable
-      t = "comment_"
-    else
-      type = object.comment.commentable.class.to_s
-      obj = object.comment.commentable
-      t = "vote_"
+    if object.trackeable.present?
+      result = true if object.trackeable.active == true
     end
     
-    t = t + "new" if type == 'Article'
-    t = t + "deb" if type == 'Debate'
-    t = t + "eve" if type == 'Event'
+    return result
     
-    newText = t(t, :var => var) if type == 'Article'
-    newText = t(t, :var => var) if type == 'Debate'
-    newText = t(t, :var => var) if type == 'Event'
-    
-    return newText
   end
   
-  def getLink(title)
-    
-    if title.length > 50
-      title.slice(0,50)+"..."
-    else
-      title
-    end    
-  end
-  
-  def isNew(object)
-    if object.class.to_s == 'Article' or object.class.to_s == 'Debate' or object.class.to_s == 'Event'
-      true
-    end
-  end
-  
-  def isComment(object)
-    if object.class.to_s == 'Comment'
-      true
-    end
-  end
-  
-  def isActive(object)
-    
-    if object.class.to_s == "Article"
-      object.published
-    elsif object.class.to_s == "Vote"
-      true
-    else
-      object.active
-    end
-  end
+  # def vote_multiple(votes,object)
+  #     count = 0
+  #     
+  #     votes.each do |vote|
+  #       if vote.comment.nil?
+  #       else
+  #         if vote.comment.commentable == object.comment.commentable
+  #           count = count + 1
+  #         end
+  #       end
+  #     end
+  #     count
+  #   end
+  #   
+  #   def getPath(object)
+  #     if object.class.to_s == 'Article' or object.class.to_s == 'Debate' or object.class.to_s == 'Event'
+  #       type = object.class.to_s
+  #       obj = object
+  #     elsif object.class.to_s == 'Comment'
+  #       type = object.commentable.class.to_s
+  #       obj = object.commentable
+  #     else
+  #       type = object.comment.commentable.class.to_s
+  #       obj = object.comment.commentable
+  #     end
+  # 
+  #     path = article_path(obj)  if type == 'Article'
+  #     path = debate_path(obj)  if type == 'Debate'
+  #     path = event_path(obj)  if type == 'Event'
+  #     
+  #     return path
+  #   end
+  #   
+  #   def getText(object,var)
+  #     if object.class.to_s == 'Article' or object.class.to_s == 'Debate' or object.class.to_s == 'Event'
+  #       type = object.class.to_s
+  #       t = "new_"
+  #       obj = object
+  #     elsif object.class.to_s == 'Comment'
+  #       type = object.commentable.class.to_s
+  #       obj = object.commentable
+  #       t = "comment_"
+  #     else
+  #       type = object.comment.commentable.class.to_s
+  #       obj = object.comment.commentable
+  #       t = "vote_"
+  #     end
+  #     
+  #     t = t + "new" if type == 'Article'
+  #     t = t + "deb" if type == 'Debate'
+  #     t = t + "eve" if type == 'Event'
+  #     
+  #     newText = t(t, :var => var) if type == 'Article'
+  #     newText = t(t, :var => var) if type == 'Debate'
+  #     newText = t(t, :var => var) if type == 'Event'
+  #     
+  #     return newText
+  #   end
+  #   
+  #   def getLink(title)
+  #     
+  #     if title.length > 50
+  #       title.slice(0,50)+"..."
+  #     else
+  #       title
+  #     end    
+  #   end
+  #   
+  #   def isNew(object)
+  #     if object.class.to_s == 'Article' or object.class.to_s == 'Debate' or object.class.to_s == 'Event'
+  #       true
+  #     end
+  #   end
+  #   
+  #   def isComment(object)
+  #     if object.class.to_s == 'Comment'
+  #       true
+  #     end
+  #   end
+  #   
+  #   def isActive(object)
+  #     
+  #     if object.class.to_s == "Article"
+  #       object.published
+  #     elsif object.class.to_s == "Vote"
+  #       true
+  #     else
+  #       object.active
+  #     end
+  #   end
   
 end
 
